47d2e9703b475259018d2b28afbdf8392975331c
pathiknd <pathikdesai@gmail.com>
2017-03-05 22:34:01 +0530

    Mock by inheriting the controller

---
+++ MockAccountsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Routing;
+
+namespace DecoratedWebApi.Mock
+{
+    public class MockAccountsController: DecoratedWebApi.AccountsController
+    {
+        public MockAccountsController(IDataProvider dataProvider)
+            :base(dataProvider)
+        { }
+
+        [Route("api/QA/accounts/{accountNumber}")]
+        public IHttpActionResult Get(string accountNumber)
+        {
+            if(accountNumber == "123456")
+            {
+                return NotFound();
+            }
+
+            return Ok(base.Get(accountNumber));
+        }
+    }
+}
